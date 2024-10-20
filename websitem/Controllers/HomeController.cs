9215b0a350f53e18e5e7c6939e4b1d609bb20eaf
Burak849 <burakkurtulus018@gmail.com>
2024-10-20 06:00:15 +0300

    eklemeler yapildi

--- websitem/Controllers/HomeController.cs
+++ websitem/Controllers/HomeController.cs
@@ -20,24 +20,6 @@ namespace websitem.Controllers
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-        public ActionResult ProfessionalSkills()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-        public ActionResult SocialEvents()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
         public ActionResult References()
         {
             ViewBag.Message = "Your contact page.";
